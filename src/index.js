4b4b637fd726eb546d018b95ff794c5fe9853ea6
Camilo Uribe Diaz <milouri23@gmail.com>
2018-02-01 12:46:13 -0500

    Add a Utility

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import _ from 'lodash';
+import { cube } from './math.js';
 
 function component() {
-    var element = document.createElement('div');
+    var element = document.createElement('pre');
 
-    // Lodash, currently included via script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'],' ');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubes is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
